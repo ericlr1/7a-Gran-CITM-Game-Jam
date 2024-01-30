fdc0bf6b53b6220a11f7840da3324f7aad0534f0
Eric <ericluque2003@gmail.com>
2024-01-30 19:18:44 +0100

    Ammo fix

--- 7a-Gran-CITM-Game-Jam/Assets/Player/UI/AmmoUI.cs
+++ 7a-Gran-CITM-Game-Jam/Assets/Player/UI/AmmoUI.cs
@@ -24,6 +24,7 @@ public class AmmoUI : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        Debug.Log("Ammo: " + script.ammo);
         switch (script.ammo)
         {
             case 0:
