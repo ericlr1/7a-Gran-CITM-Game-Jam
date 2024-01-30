a3da1a395c78b761da33186a7bc7a64be473ba9a
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2024-01-30 23:15:20 +0100

    Movment

--- 7a-Gran-CITM-Game-Jam/Assets/Player/Weapons/Weapon_Movment_Manager.cs
+++ 7a-Gran-CITM-Game-Jam/Assets/Player/Weapons/Weapon_Movment_Manager.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Weapon_Movment_Manager : MonoBehaviour
 {
     private Animator animator;
+    private bool isWPressed = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,9 +16,9 @@ public class Weapon_Movment_Manager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
+            
             animator.SetBool("Movment_Derecha", true);
         }
         if (Input.GetKeyUp(KeyCode.RightArrow))
@@ -31,7 +33,31 @@ public class Weapon_Movment_Manager : MonoBehaviour
         {
             animator.SetBool("Movment_Izquierda", false);
         }
-
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            isWPressed = true;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            isWPressed = true;
+        }
+        if (Input.GetKeyDown(KeyCode.W) && isWPressed == false)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, +0.2f);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            isWPressed = false;
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            isWPressed = false;
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            isWPressed = false;
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, -0.2f);
+        }
 
 
 
