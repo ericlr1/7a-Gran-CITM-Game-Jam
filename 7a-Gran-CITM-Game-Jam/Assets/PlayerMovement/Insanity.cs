471fab8df567eabcb084d6f027be083289290811
Eric <ericluque2003@gmail.com>
2024-01-31 17:02:59 +0100

    Insanity goes brrr
    Falta el ojo morado

--- 7a-Gran-CITM-Game-Jam/Assets/PlayerMovement/Insanity.cs
+++ 7a-Gran-CITM-Game-Jam/Assets/PlayerMovement/Insanity.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Insanity : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Referencia al Animator
+    public Animator animator;
+
+    // Valor de la cordura
+    private float insanityValue = 0.0f;
+
+    public float insanityHitValue = 1f;
+
+    // Método que se llama cuando otro Collider entra en contacto con este Collider
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
+        Debug.Log("OnTriggerEnter2D");
+        // Verificar si el otro GameObject tiene la etiqueta "Enemy"
+        if (other.CompareTag("Enemy"))
+        {
+            AddInsanity();
+            animator.SetFloat("Insanity", insanityValue);
+
+            // Aquí puedes agregar cualquier otra lógica que desees cuando un enemigo toque este objeto
+            other.gameObject.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void AddInsanity()
+    {
+        Debug.Log("AddInsanity");
+        insanityValue += insanityHitValue;
+    }
+
+    private void ReduceInsanity()
+    {
+        Debug.Log("ReduceInsanity");
+        insanityValue -= insanityHitValue;
     }
 }
