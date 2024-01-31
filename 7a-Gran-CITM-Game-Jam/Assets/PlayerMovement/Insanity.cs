a92672723dc41af01d42c84a74290c8452cc7d25
Eric <ericluque2003@gmail.com>
2024-01-31 17:22:06 +0100

    Teddy Bears

--- 7a-Gran-CITM-Game-Jam/Assets/PlayerMovement/Insanity.cs
+++ 7a-Gran-CITM-Game-Jam/Assets/PlayerMovement/Insanity.cs
@@ -15,14 +15,23 @@ public class Insanity : MonoBehaviour
     // Método que se llama cuando otro Collider entra en contacto con este Collider
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("OnTriggerEnter2D");
+       
         // Verificar si el otro GameObject tiene la etiqueta "Enemy"
         if (other.CompareTag("Enemy"))
         {
+            Debug.Log("Hit Enemy");
             AddInsanity();
             animator.SetFloat("Insanity", insanityValue);
 
             // Aquí puedes agregar cualquier otra lógica que desees cuando un enemigo toque este objeto
+            other.gameObject.SetActive(false);
+        }
+        else if (other.CompareTag("Teddy_Bear"))
+        {
+            Debug.Log("Hit Bear");
+            ReduceInsanity();
+            animator.SetFloat("Insanity", insanityValue);
+
             other.gameObject.SetActive(false);
         }
     }
@@ -35,13 +44,23 @@ public class Insanity : MonoBehaviour
 
     private void AddInsanity()
     {
-        Debug.Log("AddInsanity");
-        insanityValue += insanityHitValue;
+        if (insanityValue < 3)
+        {
+            Debug.Log("AddInsanity");
+            insanityValue += insanityHitValue;
+        }
+        else
+        {
+            //Kill the player
+        }
     }
 
     private void ReduceInsanity()
     {
-        Debug.Log("ReduceInsanity");
-        insanityValue -= insanityHitValue;
+        if (insanityValue > 0)
+        {
+            Debug.Log("ReduceInsanity");
+            insanityValue -= insanityHitValue;
+        }
     }
 }
