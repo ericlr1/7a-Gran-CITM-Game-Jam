ae0868679f6791c9247b82605654ae73efe7135a
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2024-02-01 03:42:12 +0100

    No se superpone

--- 7a-Gran-CITM-Game-Jam/Assets/Enemy/EnemyBase/AiChase.cs
+++ 7a-Gran-CITM-Game-Jam/Assets/Enemy/EnemyBase/AiChase.cs
@@ -8,10 +8,11 @@ public class AiChase : MonoBehaviour
     public float range;
 
     private float distance;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -21,11 +22,15 @@ public class AiChase : MonoBehaviour
 
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
-        direction.Normalize();       
+        direction.Normalize();
 
-        if(distance < range )
+        if (distance < range)
         {
+            // Mueve al enemigo hacia el jugador en el plano xy
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+
+            // Establece la posición en el eje z a -2
+            transform.position = new Vector3(transform.position.x, transform.position.y, -2f);
         }
     }
 }
